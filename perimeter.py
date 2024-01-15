cdc416bf446350160cae5912a040768a0b7b58ae
Кольцов Павел <p.koltsov@samolet.ru>
2024-01-15 03:10:14 +0300

    feat: Добавляет расчет площади и периметра прямоугольника

---
+++ perimeter.py
@@ -0,0 +1,8 @@
+side1 = float(input('Введите длину стороны прямоугольника: '))
+side2 = float(input('Введите ширину стороны прямоугольника: '))
+
+area = side1 * side2
+perimeter = 2 * (side1 + side2)
+
+print(f'Площадь прямоугольника: {area}')
+print(f'Периметр прямоугольника: {perimeter}')
